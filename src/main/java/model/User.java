f99dd9e75f508baa3bc5d3050b0e46d38bd8f7df
yunqili0809 <yunqili0809@gmail.com>
2022-08-13 18:39:06 -0400

    Create User.java

---
+++ src/main/java/model/User.java
@@ -0,0 +1,35 @@
+package main.java.model;
+
+import java.util.*;
+
+
+public class User  {
+	
+	LinkedList<String> messages = new LinkedList<String>();
+	
+	public static enum Role {Painter,Winner,Guesser};
+	private Role status;
+	private String username;
+	
+	public User(String name, int s) {
+		username = name;
+		if(s == 0) {
+			status = Role.Guesser;
+		}
+		else {
+			status = Role.Painter;
+		}
+	}
+	
+	public Role getRole(){
+		return status;
+	}
+	public String getUsername() {
+		return username;
+	}
+	
+	public void addNewMessage(String message) {
+		messages.addFirst(message);
+	}
+}
+
