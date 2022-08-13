c54a7b0a4e193e639c549ea0ab97e90d9755ea8d
HerobrineXia <1727193915@qq.com>
2022-08-13 18:06:48 -0400

    File structure update

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,23 @@
+package main.java;
+
+// import main.java.control.GameControl;
+
+/**
+* The launcher of the game
+* @author Kevin Xia
+* @version 1.0
+*/
+public class Launcher {
+    /**
+     * Launcher of the Game
+     * @param args None
+     */
+    public static void main(String[] args){
+        // try{
+        //     new GameControl();
+        // }catch(Exception e){
+        //     e.printStackTrace();
+        // }
+        System.out.println("Hello World");
+    }
+}
