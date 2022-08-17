b546414ad012c1eeb9a998a85f23c072bb1c3837
yunqili0809 <yunqili0809@gmail.com>
2022-08-17 17:12:50 -0400

    protocol

--- src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/control/Protocol.java
+++ src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/control/Protocol.java
@@ -23,12 +23,58 @@ public class Protocol {
 	private String[] parseCommand(String command){
         return command.split(SEPARATOR);
     }
+	
+	public String unicodeEscaped(char ch) {
+	      if (ch < 0x10) {
+	          return "\\u000" + Integer.toHexString(ch);
+	      } else if (ch < 0x100) {
+	          return "\\u00" + Integer.toHexString(ch);
+	      } else if (ch < 0x1000) {
+	          return "\\u0" + Integer.toHexString(ch);
+	      }
+	      return "\\u" + Integer.toHexString(ch);
+	  }
+	
+	public String stringToUnicode(String message) {
+		StringBuilder response = new StringBuilder();
+		for(int i=0;i<message.length();i++) {
+			if(Character.isAlphabetic(message.charAt(i))) {
+				response.append(new StringBuilder(unicodeEscaped(message.charAt(i))));
+			}
+			else {
+				if(message.charAt(i) == ' ') {
+					response.append(' ');
+				}
+				else {
+					response.append(message.charAt(i));
+				}		
+			}
+		}
+		return response.toString();
+	}
+	
+	public String unicodeToString(String message) {
+		String str = message;
+		str = str.replace("\\","");
+		String[] arr = str.split("u");
+		String text = "";
+		for(int i = 1; i < arr.length; i++){
+			if(arr[i].length()>4) {
+				int hexVal = Integer.parseInt(arr[i].substring(0, 4), 16);
+				text += (char)hexVal;
+				text+= arr[i].substring(4);
+			}
+			else {
+				int hexVal = Integer.parseInt(arr[i].substring(0, 4), 16);
+				text += (char)hexVal;
+			}
 
-	public String transferString(String message){
-		return message;
+		}
+		return text;
 	}
 
 	public String messagePack(int id, String message){
+		message = stringToUnicode(message);
 		StringBuilder response = new StringBuilder("%s%s%s%s%d%s%s".formatted("DATA",SEPARATOR,"MESSAGE",SEPARATOR,id,SEPARATOR,message));
 		return response.toString();
 	}
@@ -45,6 +91,7 @@ public class Protocol {
 		return response.toString();
 	}
 	public String userJoinServerEvent(String address, String name) {
+		name = stringToUnicode(name);
 		StringBuilder response = new StringBuilder("%s%s%s%s%s%s%s".formatted("EVENT",SEPARATOR,"JOIN_SERVER",SEPARATOR,name,SEPARATOR,address));
 		return response.toString();
 	}
@@ -53,6 +100,7 @@ public class Protocol {
 		return response.toString();
 	}
 	public String userJoinClientEvent(int id, String name) {
+		name = stringToUnicode(name);
 		StringBuilder response = new StringBuilder("%s%s%s%s%d%s%s".formatted("EVENT",SEPARATOR,"JOIN_CLIENT",SEPARATOR,id,SEPARATOR,name));
 		return response.toString();
 	}
@@ -69,7 +117,6 @@ public class Protocol {
 	public String process(String command){
         StringBuilder response = new StringBuilder();
 		System.out.println(command);
-        command = transferString(command);
         String[] commands = parseCommand(command);
         String keyword = commands[0];
         String secondary = commands[1];
@@ -86,6 +133,7 @@ public class Protocol {
 			else if(secondary.equals("JOIN_CLIENT")) {
 				int id = Integer.parseInt(commands[2]);
 				String name = commands[3];
+				name = unicodeToString(name);
 				controller.playerJoinEventClient(name, id);
 			}
 			else if(secondary.equals("SENT")) {
@@ -99,7 +147,7 @@ public class Protocol {
 				
 			}
 			else if(secondary.equals("NEW_ROUND")) {
-				int startId = Integer.parseInt(commands[2]);
+				int painterId = Integer.parseInt(commands[2]);
 				//controller
 			}
 			else {
@@ -114,12 +162,13 @@ public class Protocol {
 			else if(secondary.equals("MESSAGE")) {
 				int id = Integer.parseInt(commands[2]);
 				String message = commands[3];
+				message = unicodeToString(message);
 				// controller
 			}
 			else if(secondary.equals("MODEL")) {
 				LinkedList <User> users = new LinkedList <User>(); 
 				for(int i = 3;i<Integer.parseInt(commands[2]);i+=3) {
-					users.addLast(new User(commands[i+1],Integer.parseInt(commands[i+2]),Integer.parseInt(commands[i])));
+					users.addLast(new User( unicodeToString(commands[i+1]),Integer.parseInt(commands[i+2]),Integer.parseInt(commands[i])));
 				}
 				//controller
 			}
