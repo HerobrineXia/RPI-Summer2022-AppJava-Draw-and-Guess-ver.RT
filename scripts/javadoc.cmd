726b5872e99628e588ff4957ae107064aaf0ff5d
yunqili0809 <yunqili0809@gmail.com>
2022-08-19 15:21:34 -0400

    read me and scripts

---
+++ scripts/javadoc.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+cd ..
+javadoc -cp ".\lib\flatlaf-2.4.jar;.\src"  -author -version -encoding UTF-8 -sourcepath .\src edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess -d .\docs
+cd scripts
+
