1693f3874cc3ffaedbbc0117e1bfb2013cea60ac
HerobrineXia <1727193915@qq.com>
2022-08-19 01:18:23 -0400

    Update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 echo "[MSG] Compiling..."
 cd ..
 javac -cp ./lib/flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java -d bin
-java -classpath ./src:./lib/flatlaf-2.4.jar edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess.Launcher
+java -classpath ./bin:./lib/flatlaf-2.4.jar edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess.Launcher
 cd scripts
