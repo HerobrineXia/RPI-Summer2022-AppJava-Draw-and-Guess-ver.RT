60541b3de9cdba13b8e13529242a28d316ee21e5
yunqili0809 <yunqili0809@gmail.com>
2022-08-19 00:17:58 -0400

    1

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,5 @@
+echo "[MSG] Compiling..."
+cd ..
+javac -cp ./lib/flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java
 java -classpath ./src:./lib/flatlaf-2.4.jar edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess.Launcher
+cd scripts
