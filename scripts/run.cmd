0f1cc6197309e987d70612cee77d2b9edef06698
HerobrineXia <1727193915@qq.com>
2022-08-19 00:52:28 -0400

    Update

--- scripts/run.cmd
+++ scripts/run.cmd
@@ -2,7 +2,7 @@
 @ECHO OFF
 cd ..
 echo "Compiling..."
-javac -cp .\lib\flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java
+javac -cp .\lib\flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java -d bin
 
 echo "Press enter to let the application start"
 PAUSE >nul
