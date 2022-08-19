dbd109ef06f12c1ab5bcfda0b795863585d491a0
yunqili0809 <yunqili0809@gmail.com>
2022-08-19 00:16:35 -0400

    script

--- scripts/compile.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-echo "Compiling..."
-javac -cp .\lib\flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java
-
--- scripts/run.cmd
+++ scripts/run.cmd
@@ -1,2 +1,10 @@
 @ECHO OFF
+@ECHO OFF
+cd ..
+echo "Compiling..."
+javac -cp .\lib\flatlaf-2.4.jar -sourcepath src src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/Launcher.java
+
+echo "Press enter to let the application start"
+PAUSE >nul
 java -classpath .\src;.\lib\flatlaf-2.4.jar edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess.Launcher
+cd scripts
