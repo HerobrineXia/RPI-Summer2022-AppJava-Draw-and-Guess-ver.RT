1bca08bd48a0c5c8ab830cd2d3d273d17ebc5a16
Yuetian Chen <31975605+Stry233@users.noreply.github.com>
2022-08-21 13:01:29 -0400

    2022.08.21: add util specification

--- src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/tools/SystemCheck.java
+++ src/edu/rpi/cs/csci4963/u22/cheny63/project/drawAndGuess/tools/SystemCheck.java
@@ -1,10 +1,24 @@
 package edu.rpi.cs.csci4963.u22.cheny63.project.drawAndGuess.tools;
 
+/**
+ * SystemCheck is the utility for checking the current system type
+ * 
+ * @author Yuetian Chen
+ * @version <b>1.0</b> rev. 0
+ */
 public class SystemCheck {
+	/**
+	 * check if the current platform is a version of unix-based max os 
+	 * @return true if current system is mac os
+	 */
 	public static boolean isMacintosh() {
 		return System.getProperty("os.name").startsWith("Mac");
 	}
 
+	/**
+	 * check if the current platform is a version of windows NT
+	 * @return true if current system is windows
+	 */
 	public static boolean isWindows() {
 		return System.getProperty("os.name").startsWith("Windows");
 	}
